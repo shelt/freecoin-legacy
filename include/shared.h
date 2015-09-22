1937f235d770bce3bc464b2626fc405c5c07e842
Sam Shelton <sam@shelt.ca>
2015-09-22 09:07:37 -0500

    Fix macro comments

--- include/shared.h
+++ include/shared.h
@@ -1,19 +1,23 @@
 #ifndef SHARED_H
+
 #define SHARED_H
 
-#define unchar unsigned char 
+#define __VERSION "0.1a"
+
+#define unchar unsigned char
 #define unint unsigned int
 #define unshort unsigned short
 
-#define __version 1
+// The same as sizeof(Tx_header)
+#define TX_HEADER_SIZE 10
 
-#define TX_HEADER_SIZE 10      // The same as sizeof(Tx_header)
-#define TX_INPUT_BYTESIZE 160  // The bytesize of 1 input.  [IN_TX .. SIG]  32B + 128B
-#define TX_OUTPUT_BYTESIZE 132 // The bytesize of 1 output. [PUBKEY .. AMT] 128B + 4B
+// The bytesize of 1 input.  [IN_TX .. SIG]  32B + 128B    
+#define TX_INPUT_BYTESIZE 160
+
+// The bytesize of 1 output. [PUBKEY .. AMT] 128B + 4B
+#define TX_OUTPUT_BYTESIZE 132
 
 #define SHA256_SIZE 32
 #define RSA1024_SIZE 128
-
-
 
 #endif
