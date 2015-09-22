fe45855750cc7fccc3a9ac6b41c07a16bacbc0c2
Sam Shelton <sam@shelt.ca>
2015-09-22 09:38:55 -0500

    Add include guards

--- include/crypto.h
+++ include/crypto.h
@@ -1,5 +1,7 @@
 #include "shared.h"
 
+#ifndef CRYPTO_H
+#define CRYPTO_H
 
 
 typedef struct 
@@ -16,3 +18,5 @@ void sha256_init(SHA256_CTX *ctx);
 void sha256_update(SHA256_CTX *ctx, unchar data[], size_t len);
 
 void sha256_final(SHA256_CTX *ctx, unchar hash[]);
+
+#endif
