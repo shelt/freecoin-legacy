399e3cd5a33da4ce1d839a443023f1aa9c8ebf15
Sam Shelton <sam@shelt.ca>
2016-01-19 23:46:29 -0600

    Remove old files

--- old-include/printing.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINTING_H
-#define PRINTING_H
-
-void print_greeting();
-void print_version();
-void printfv(int verbose, const char *format, ...);
-void die(const char *string, ...);
-
-#endif
